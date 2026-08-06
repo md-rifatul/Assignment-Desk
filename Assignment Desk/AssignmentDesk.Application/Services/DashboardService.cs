9b640a46b2577e7536dd8eaab8774c9416b22bb4
rifatul <rifatul.ism@gmail.com>
2026-08-06 15:46:11 +0600

    IDashboardService and DashboardService create

---
+++ Assignment Desk/AssignmentDesk.Application/Services/DashboardService.cs
@@ -0,0 +1,28 @@
+﻿using AssignmentDesk.Application.Auth.DTOs;
+using AssignmentDesk.Application.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDesk.Application.Services
+{
+    public class DashboardServicen : IDashboardService
+    {
+        public Task<AdminDashboardDto> GetAdminDashboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<StudentDashboardDto> GetStudentDashboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TeacherDashboardDto> GetTeacherDashboard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
