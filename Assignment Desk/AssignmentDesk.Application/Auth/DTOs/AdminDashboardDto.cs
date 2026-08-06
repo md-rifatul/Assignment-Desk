2976e250882eacebb5f6149c8b9743fe2ae96162
rifatul <rifatul.ism@gmail.com>
2026-08-06 15:02:58 +0600

    Created Admin, Teacher, StudentDto, and DashboardController

---
+++ Assignment Desk/AssignmentDesk.Application/Auth/DTOs/AdminDashboardDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDesk.Application.Auth.DTOs
+{
+    public class AdminDashboardDto
+    {
+        public int TotalTeachers { get; set; }
+        public int TotalStudents { get; set; }
+        public int TotalClasses { get; set; }
+        public int TotalSubjects { get; set; }
+        public int TotalAssignments { get; set; }
+    }
+}
---
+++ Assignment Desk/AssignmentDesk.Application/Auth/DTOs/StudentDashboardDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDesk.Application.Auth.DTOs
+{
+    public class StudentDashboardDto
+    {
+        public int MySubjects { get; set; }
+        public int MyAssignments { get; set; }
+        public int SubmissionStatus { get; set; }
+    }
+}
