ada5b2716d16d84212074a62212ee27de9d076c4
rifatul <rifatul.ism@gmail.com>
2026-08-09 12:40:49 +0600

    Authorization add on DashboardController

--- Assignment Desk/Assignment Desk/Controllers/DashboardController.cs
+++ Assignment Desk/Assignment Desk/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using AssignmentDesk.Application.Interfaces.IServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -15,11 +16,13 @@ namespace Assignment_Desk.Controllers
             _dashboardService = dashboardService;
         }
         [HttpGet("admin")]
+        [Authorize(Roles ="Admin")]
         public async Task<IActionResult> AdminDashboard()
         {
              return Ok(await _dashboardService.GetAdminDashboard());
         }
         [HttpGet("teacher")]
+        [Authorize(Roles = "Teacher")]
         public async Task<IActionResult> TeacherDashboard()
         {
             int teacherId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
