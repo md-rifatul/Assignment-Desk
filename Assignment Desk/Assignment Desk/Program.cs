519215b440693956fdd8563ef4184d2141e23f61
rifatul <rifatul.ism@gmail.com>
2026-08-13 04:41:59 +0600

    Fix health check content type to HTML

--- Assignment Desk/Assignment Desk/Program.cs
+++ Assignment Desk/Assignment Desk/Program.cs
@@ -168,7 +168,7 @@ using (var scope = app.Services.CreateScope())
 
 
 //health check
-app.MapGet("/health", () => Results.Text("OK"));
+app.MapGet("/health", () => Results.Content("<h1>API is Live</h1>", "text/html"));
 
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
